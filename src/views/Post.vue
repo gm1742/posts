279830fedba098b06c76b6a884a0e35bdbd71765
Misha <speed17gm@gmail.com>
2021-02-20 10:29:28 +0200

    first commit

---
+++ src/views/Post.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h2>{{title}}titledsfdsf</h2>
+    <div class="d-flex">
+      <div>User</div>
+      <div>{{userId}}</div>
+    </div>
+    <div class="text">
+      {{body}}
+    </div>
+  </div>
+</template>
+
+<script>
+   export default {
+      props: ['title', 'userId', 'body']
+   }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Posts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container pt-5">
+    <todo-list
+      title="Title"
+      body="text"
+    ></todo-list>
+  </div>
+</template>
+
+<script>
+
+import TodoList from "../components/TodoList";
+
+export default {
+   components: { TodoList }
+}
+</script>
